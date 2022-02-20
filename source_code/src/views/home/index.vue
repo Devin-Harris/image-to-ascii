c0dbe0f29e5fea515a0c0eefc108b23e2f6e03f3
Devin Harris <devinharris.316@gmail.com>
2022-02-19 19:36:09 -0500

    continued setup vue project for new ui

--- source_code/src/views/home/index.vue
+++ source_code/src/views/home/index.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <style src="./home.scss" lang="scss" scoped></style>
-
 <script src="./home.ts" lang="ts"></script>
