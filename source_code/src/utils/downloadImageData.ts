3b6f0d50b644b0c9661246d5ae1ac12fea615537
Devin Harris <devinharris.316@gmail.com>
2022-02-27 12:48:05 -0500

    allowing download formatting option

--- source_code/src/utils/downloadImageData.ts
+++ source_code/src/utils/downloadImageData.ts
@@ -1,13 +1,34 @@
 // @ts-ignore
 import domtoimage from "dom-to-image-more";
 
-export function downloadImageFromHtmlElement(elm: HTMLElement, imageName = 'image.jpeg') {
-  return domtoimage
-    .toJpeg(elm, { quality: 0.95 })
-    .then((dataUrl: string) => {
-      var link = document.createElement("a");
-      link.download = imageName;
-      link.href = dataUrl;
-      link.click();
-    });
+export function downloadImageFromHtmlElement(elm: HTMLElement, format = 'png', imageName = 'download') {
+  return new Promise((resolve, reject) => {
+    try {
+      if (format === 'png') {
+        domtoimage
+          .toPng(elm)
+          .then((dataUrl: string) => {
+            var link = document.createElement("a");
+            link.download = imageName + '.' + format;
+            link.href = dataUrl;
+            link.click();
+            resolve('Downloaded Image')
+          });
+      } else if (format === 'jpeg' || format === 'jpg') {
+        domtoimage
+          .toJpeg(elm)
+          .then((dataUrl: string) => {
+            var link = document.createElement("a");
+            link.download = imageName + '.' + format;
+            link.href = dataUrl;
+            link.click();
+            resolve('Downloaded Image')
+          });
+      } else {
+        throw 'Invalid Format'
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
